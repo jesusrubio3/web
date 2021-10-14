55d7e2ef8b29d3619299bc618e0a751ec0a02b12
jesus_rubio <jesusrubiomartin3@gmail.com>
2021-10-14 08:29:55 +0200

    cambio script

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6b8d205290dd215dcf4b";
+/******/ 	var hotCurrentHash = "b24f341f32dc8e7bb446";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
